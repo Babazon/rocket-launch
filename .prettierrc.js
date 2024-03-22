4bbbf41c92d4d423e8a39f0c321d9f78647c3d76
Başar Yüksel <basar@stadion.io>
2024-03-22 22:51:43 +0100

    Set up quality of life improvements

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
   singleQuote: true,
+  semi: false,
+  tabWidth: 2,
   trailingComma: 'all',
-};
+  printWidth: 100,
+  arrowParens: 'avoid',
+}
